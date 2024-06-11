cbe80430cfc7053f56414093c82127b22c72b189
AutoDL <2743880348@qq.com>
2024-06-11 13:38:36 +0800

    update setup.py

--- data_loader/setup.py
+++ data_loader/setup.py
@@ -3,9 +3,9 @@ import numpy as np
 
 coviar_utils_module = Extension('coviar',
 		sources = ['coviar_data_loader.c'],
-		include_dirs=[np.get_include(), './ffmpeg/include/'],
+		include_dirs=[np.get_include(), './ffmpeg/include/', '/usr/local/ffmpeg/include/'],
 		extra_compile_args=['-DNDEBUG', '-O3'],
-		extra_link_args=['-lavutil', '-lavcodec', '-lavformat', '-lswscale', '-L./ffmpeg/lib/']
+		extra_link_args=['-lavutil', '-lavcodec', '-lavformat', '-lswscale', '-L/usr/local/ffmpeg/lib/']
 )
 
 setup ( name = 'coviar',
